7bfcaed618da654383454c7dd58cf2e847364941
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2024-06-21 16:48:01 -0400

    more pr changes

--- sensor/sensor.go
+++ sensor/sensor.go
@@ -75,9 +75,10 @@ func newFailoverSensor(ctx context.Context, deps resource.Dependencies, conf res
 	for _, backup := range config.Backups {
 		backup, err := sensor.FromDependencies(deps, backup)
 		if err != nil {
-			return nil, err
+			s.logger.Errorf(err.Error())
+		} else {
+			s.backups = append(s.backups, backup)
 		}
-		s.backups = append(s.backups, backup)
 	}
 
 	s.lastWorkingSensor = primary
@@ -123,16 +124,16 @@ func getReading(ctx context.Context, sensor resource.Sensor, extra map[string]in
 }
 
 func (s *failoverSensor) tryReadingOrFail(ctx context.Context, sensor sensor.Sensor, extra map[string]interface{}) (map[string]interface{}, error) {
-	result := make(chan readingsResult, 1)
+	resultChan := make(chan readingsResult, 1)
 	go func() {
-		result <- getReading(ctx, sensor, extra)
+		resultChan <- getReading(ctx, sensor, extra)
 	}()
 	select {
 	case <-time.After(time.Duration(s.timeout * 1e6)):
 		return nil, fmt.Errorf("%s timed out", sensor.Name())
-	case result := <-result:
+	case result := <-resultChan:
 		if result.err != nil {
-			return nil, fmt.Errorf("sensor %s failed to get readings: %s", sensor.Name(), result.err.Error())
+			return nil, fmt.Errorf("sensor %s failed to get readings: %w", sensor.Name(), result.err)
 		} else {
 			return result.readings, nil
 		}
@@ -140,49 +141,45 @@ func (s *failoverSensor) tryReadingOrFail(ctx context.Context, sensor sensor.Sen
 }
 
 func (s *failoverSensor) Readings(ctx context.Context, extra map[string]interface{}) (map[string]interface{}, error) {
+	// Poll the last sensor we know is working
 	reading, err := s.tryReadingOrFail(ctx, s.lastWorkingSensor, extra)
 	if reading != nil {
 		return reading, nil
 	}
+	// upon error of the last working sensor, log returned error.
 	s.logger.Warnf(err.Error())
 
-	// if the primary failed, start goroutine to check for it to get readings again.
+	// Lock the mutex to protect lastWorkingSensor from changing before the readings call finishes.
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	// If the primary failed, start goroutine to check for it to get readings again.
 	switch s.lastWorkingSensor {
 	case s.primary:
+		// Check if the primary comes back online and replace the lastWorkingSensor
+		// with primary when it does.
 		s.pollPrimaryForHealth(extra)
 	default:
 	}
 
+	// Start reading from the list of backup sensors until one succeeds.
 	for _, backup := range s.backups {
-		// Lock the mutex to protect lastWorkingSensor from changing before the readings call finishes.
-		s.mu.Lock()
 		// if the last working sensor is a backup, it was already tried above.
 		if s.lastWorkingSensor == backup {
 			continue
 		}
-		s.mu.Unlock()
 		s.logger.Infof("calling backup %s", backup.Name())
 		reading, err := s.tryReadingOrFail(ctx, backup, extra)
 		if err != nil {
-			s.logger.Warnf(err.Error())
+			s.logger.Warn(err.Error())
 		} else {
 			s.logger.Infof("successfully got reading from %s", backup.Name())
-			s.mu.Lock()
 			s.lastWorkingSensor = backup
-			s.mu.Unlock()
 			return reading, nil
 		}
 	}
 	// couldn't get reading from any sensors.
-	return nil, fmt.Errorf("failover %s: all sensors failed to get readings", s.Named)
-}
-
-// Close closes the sensor.
-func (s *failoverSensor) Close(ctx context.Context) error {
-	if s.workers != nil {
-		s.workers.Stop()
-	}
-	return nil
+	return nil, fmt.Errorf("all sensors failed to get readings")
 }
 
 // pollPrimaryForHealth starts a background routine that continuously polls the primary sensor until it returns a reading.
@@ -206,4 +203,12 @@ func (s *failoverSensor) pollPrimaryForHealth(extra map[string]interface{}) {
 			}
 		}
 	})
+}
+
+// Close closes the sensor.
+func (s *failoverSensor) Close(ctx context.Context) error {
+	if s.workers != nil {
+		s.workers.Stop()
+	}
+	return nil
 }
